a36da1c67c8b3be0c1985e9878da7ec68edbb8c7
Евгений Буренок <eburenoq@yandex.ru>
2020-03-07 19:13:33 +0300

    commit

---
+++ AdoNET/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace AdoNET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         
+
+            string connectionString = @"Data Source = LAPTOP-B3D7R5IQ;Initial Catalog = TestERwin; Integrated Security = True";
+
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            try
+            {
+                connection.Open();
+                Console.WriteLine(connection.State);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+
+            }
+
+        }
+    }
+}
